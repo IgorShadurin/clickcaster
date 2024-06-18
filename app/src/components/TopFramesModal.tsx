79d381f68bc6fc723ce1d47f1853c8c14e4dfb2b
Igor Shadurin <igor.shadurin@gmail.com>
2024-06-18 13:53:11 +0100

    feat: display stata

--- app/src/components/TopFramesModal.tsx
+++ app/src/components/TopFramesModal.tsx
@@ -23,16 +23,17 @@ export function TopFramesModal({ show, handleClose }: { show: boolean; handleClo
     <Modal
       show={show}
       onHide={handleClose}
-      size={'lg'}
+      size="lg"
     >
       <Modal.Header closeButton>
         <Modal.Title>Top Frames</Modal.Title>
       </Modal.Header>
-      <Modal.Body>
+      <Modal.Body style={{ maxHeight: '400px', overflowY: 'auto' }}>
         {stata ? (
-          <Table striped bordered hover>
+          <Table bordered hover>
             <thead>
             <tr>
+              <th>#</th>
               <th>Owner ID</th>
               <th>Frame ID</th>
               <th>Unique Visitors</th>
@@ -40,16 +41,25 @@ export function TopFramesModal({ show, handleClose }: { show: boolean; handleClo
             </tr>
             </thead>
             <tbody>
-            {stata.sortedOwnerIds.map(ownerId => (
-              Object.entries(stata.stata[ownerId]).map(([frameId, stats]) => (
+            {stata.sortedOwnerIds.map((ownerId, ownerIndex) => {
+              const frames = Object.entries(stata.stata[ownerId])
+              // Sort frames by unique visitors in descending order
+              frames.sort((a, b) => b[1].unique - a[1].unique)
+
+              return frames.map(([frameId, stats], index) => (
                 <tr key={`${ownerId}-${frameId}`}>
-                  <td>{ownerId}</td>
+                  {index === 0 && (
+                    <>
+                      <td rowSpan={frames.length}>{ownerIndex + 1}</td>
+                      <td rowSpan={frames.length}>{ownerId}</td>
+                    </>
+                  )}
                   <td>{frameId}</td>
                   <td>{stats.unique}</td>
                   <td>{stats.all}</td>
                 </tr>
               ))
-            ))}
+            })}
             </tbody>
           </Table>
         ) : (
